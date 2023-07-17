bc76ed21c840e04efad6950f6145991888bd91e4
Symoon Zadid <symoon.zadid@brainstation-23.com>
2023-07-17 22:22:41 +0600

    Initial commit

---
+++ src/main/java/com/Spring/Boot/Actuator/and/Metrics/SpringBootActuatorAndMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.Spring.Boot.Actuator.and.Metrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActuatorAndMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootActuatorAndMetricsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Spring/Boot/Actuator/and/Metrics/SpringBootActuatorAndMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring.Boot.Actuator.and.Metrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActuatorAndMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
